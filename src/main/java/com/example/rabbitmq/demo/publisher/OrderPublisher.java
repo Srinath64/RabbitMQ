0155a77ef2bac6dd1dc72263f25d7d41d10e50cb
Srinath S <srinath@Srinaths-MacBook-Air.local>
2022-06-24 17:45:51 +0530

    Added : RabbitMQ

---
+++ src/main/java/com/example/rabbitmq/demo/publisher/OrderPublisher.java
@@ -0,0 +1,26 @@
+package com.example.rabbitmq.demo.publisher;
+
+import com.example.rabbitmq.demo.config.MessagingConfig;
+import com.example.rabbitmq.demo.dto.Order;
+import com.example.rabbitmq.demo.dto.OrderStatus;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/order")
+public class OrderPublisher {
+
+    @Autowired
+    private RabbitTemplate template;
+
+    @PostMapping("/{restaurantName}")
+    public String bookOrder(@RequestBody Order order, @PathVariable String restaurantName) {
+        order.setOrderId(UUID.randomUUID().toString());
+        OrderStatus orderStatus = new OrderStatus(order, "PROCESS", "Order placed successfully in " + restaurantName);
+        template.convertAndSend(MessagingConfig.EXCHANGE, MessagingConfig.ROUTING_KEY, orderStatus);
+        return "Success !!";
+    }
+}
